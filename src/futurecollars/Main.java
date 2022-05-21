743a6479f887356dee159b1f338c2fcc437cd77e
Pawel Przybylski <pawel.przybylski1989@gmail.com>
2022-05-21 15:07:46 +0200

    add new  print line

--- src/futurecollars/Main.java
+++ src/futurecollars/Main.java
@@ -3,5 +3,6 @@ package futurecollars;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("New line added");
     }
 }
